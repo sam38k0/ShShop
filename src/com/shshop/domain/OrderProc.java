5a22382167f08cee36a7e109816ebb8527faf675
Sam <kin3303@gmail.com>
2015-06-22 11:36:08 +0900

    OrderMapper 수정

--- src/com/shshop/domain/OrderProc.java
+++ src/com/shshop/domain/OrderProc.java
@@ -1,5 +1,7 @@
 package com.shshop.domain;
 
+import java.sql.Date;
+
 public class OrderProc {
 	private Order order;
 	private Integer insertedOrderId;
@@ -22,5 +24,85 @@ public class OrderProc {
 
 	public void setInsertedOrderId(Integer insertedOrderId) {
 		this.insertedOrderId = insertedOrderId;
+	}
+	
+	public Integer getOrderId() {
+		return order.getOrderId();
+	}
+
+	public void setOrderId(Integer orderId) {
+		order.setOrderId(orderId);
+	}
+
+	public Integer getUserId() {
+		return order.getUserId();
+	}
+
+	public void setUserId(Integer userId) {
+		order.setUserId(userId);
+	}
+
+	public Integer getProductId() {
+		return order.getProductId();
+	}
+
+	public void setProductId(Integer productId) {
+		order.setProductId(productId);
+	}
+
+	public Integer getIdAddress() {
+		return order.getIdAddress();
+	}
+
+	public void setIdAddress(Integer idAddress) {
+		order.setIdAddress(idAddress);
+	}
+
+	public Integer getAmount() {
+		return order.getAmount();
+	}
+
+	public void setAmount(Integer amount) {
+		order.setAmount(amount);
+	}
+
+	public Integer getCost() {
+		return order.getCost();
+	}
+
+	public void setCost(Integer cost) {
+		order.setCost(cost);
+	}
+
+	public Integer getDeliveryCost() {
+		return order.getDeliveryCost();
+	}
+
+	public void setDeliveryCost(Integer deliveryCost) {
+		order.setDeliveryCost(deliveryCost);
+	}
+
+	public String getOrderRequest() {
+		return order.getOrderRequest();
+	}
+
+	public void setOrderRequest(String orderRequest) {
+		order.setOrderRequest(orderRequest);
+	}
+
+	public Date getDateCreated() {
+		return order.getDateCreated();
+	}
+
+	public void setDateCreated(Date dateCreated) {
+		order.setDateCreated(dateCreated);
+	}
+
+	public Date getDateUpdated() {
+		return order.getDateUpdated();
+	}
+
+	public void setDateUpdated(Date dateUpdated) {
+		order.setDateUpdated(dateUpdated);
 	}
 }
