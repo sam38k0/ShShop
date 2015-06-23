dc1b676fe4a627ed46db2c2efa527e5e6c329f88
Sam <kin3303@gmail.com>
2015-06-23 19:30:56 +0900

    OrderView - Page 처리 추가

--- src/com/shshop/command/OrderCommand.java
+++ src/com/shshop/command/OrderCommand.java
@@ -12,6 +12,8 @@ import com.shshop.control.CommandResult;
 import com.shshop.domain.Address;
 import com.shshop.domain.OrderState;
 import com.shshop.domain.User;
+import com.shshop.helper.Format;
+import com.shshop.response.OrderInfo;
 import com.shshop.response.OrderViewInfo;
 import com.shshop.service.AuthenticatorService;
 import com.shshop.service.ProductService;
@@ -35,11 +37,13 @@ public class OrderCommand implements Command {
 
 		OrderViewInfo orderViewInfo = new OrderViewInfo(user, addresses, 1, 5);
 		ProductService productService = new ProductService();
-		 
-		orderViewInfo.addOrderInfo(productService.createNewOrderInfo(request, user.getUserId(), 1, 3, 2500, "총알배송",  OrderState.VirtualOrder));
-		orderViewInfo.addOrderInfo(productService.createNewOrderInfo(request, user.getUserId(), 2, 3, 5000, "당일배송",  OrderState.VirtualOrder));
-		orderViewInfo.addOrderInfo(productService.createNewOrderInfo(request, user.getUserId(), 3, 3, 10000, "파손주의", OrderState.VirtualOrder));
 
+		for(int i = 0; i < 102; i++) {
+			OrderInfo orderInfo = productService.createNewOrderInfo(request, user.getUserId(), i, Format.randBetween(1, 10), Format.randBetween(2500, 5000), "주의", OrderState.VirtualOrder);
+			if(orderInfo != null)
+				orderViewInfo.addOrderInfo(orderInfo);
+		}		
+		
 		String orderKey = UUID.randomUUID().toString();
 
 		request.setAttribute(Constant.attrOrderViewInfo, orderViewInfo);
